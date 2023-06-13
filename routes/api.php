5a922dcefb7cc0296254b44f33693dea2f42972d
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-13 13:54:26 +0200

    Added routes in Api.php + Created API Controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+ */
+
+ Route::resource('projects', ProjectController::class)->parameters(['projects' => 'project:slug'])->only(['index', 'show']);
