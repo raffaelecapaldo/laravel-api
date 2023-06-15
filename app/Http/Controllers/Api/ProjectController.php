c2a11a416d1b9d65441565cf9614ab7202834523
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-15 13:48:53 +0200

    Show 20 projects for page

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::latest()->with('languages')->paginate(5);
+        $projects = Project::latest()->with('languages')->paginate(20);
         if (!$projects) {
             return response()->json([
                 'success' => false,
